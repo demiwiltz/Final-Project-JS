efbeff2d6546a4a90a802bed0af9a44329a937a6
Demi <demiwiltz25@gmail.com>
2025-05-12 16:09:14 -0500

    adding new project

---
+++ index.js
@@ -0,0 +1,9 @@
+// fetch("http://rest.nbaapi.com/api/PlayerDataAdvanced/name/jayson%20Tatum")
+
+// 1. create web page using html & css, 
+// 2. make search engine function w/js by showing the first three images on the page
+// 3. get the skeleton loading state to show on the page
+
+
+// category:
+// action/adventure (transformers), kids (toy story)
