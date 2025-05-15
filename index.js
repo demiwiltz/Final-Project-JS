3c89202102b95ae253be974157886afca8c50304
Demi <demiwiltz25@gmail.com>
2025-05-15 18:24:10 -0500

    updated js project

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-// fetch("http://rest.nbaapi.com/api/PlayerDataAdvanced/name/jayson%20Tatum")
-
 // 1. create web page using html & css, 
 // 2. make search engine function w/js by showing the first three images on the page
 // 3. get the skeleton loading state to show on the page
-
-
 // category:
 // action/adventure (transformers), kids (toy story)
+
+// E - Commerce App Video {5:28}
+
+function renderMovies() {
+    const movieWrapper = document.querySelector('.movie')
+console.log(movieWrapper) 
+`<div class="movie">
+    <figure>
+    <img class="movie__img" src="https://m.media-amazon.com/images/M/MV5BZjM3ZDA2YmItMzhiMi00ZGI3LTg3ZGQtOTk3Nzk0MDY0ZDZhXkEyXkFqcGc@._V1_SX300.jpg" alt="">
+    </figure> 
+    <h4 class="movie__title">Transformers</h4>
+</div>`
+}
+
+setTimeout(() => {
+    renderMovies();
+}, 2000);
+
+
+
+
